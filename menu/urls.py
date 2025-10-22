123aa3e938d2dc05d1cc74a19fc7e76203b6feaa
Muhammad Shuvo Khan <86629505+shuvokhan60@users.noreply.github.com>
2025-10-22 11:44:50 +0600

    update path

--- menu/urls.py
+++ menu/urls.py
@@ -3,25 +3,19 @@ from . import views
 
 urlpatterns = [
 
-    # ==========================
-    # 🎓 STUDENT SIDE
-    # ==========================
+
     path('student/menu/', views.student_menu, name='student_menu'),
     path('student/order/<int:item_id>/', views.order_food, name='order_food'),
     path('student/orders/', views.student_orders, name='student_orders'),
     path('student/pay/<int:order_id>/', views.make_payment, name='make_payment'),
 
-    # ==========================
-    # 👨‍🍳 STAFF SIDE (MENU MANAGEMENT)
-    # ==========================
+   
     path('staff/menu/', views.staff_menu, name='staff_menu'),
     path('staff/menu/add/', views.add_food, name='add_food'),
     path('staff/menu/edit/<int:food_id>/', views.edit_food, name='edit_food'),
     path('staff/menu/delete/<int:food_id>/', views.delete_food, name='delete_food'),
 
-    # ==========================
-    # 🧾 STAFF SIDE (MANAGE ORDERS)
-    # ==========================
+   
     path('staff/orders/', views.manage_orders, name='manage_orders'),
     path('staff/orders/update/<int:order_id>/', views.update_order_status, name='update_order_status'),
 
