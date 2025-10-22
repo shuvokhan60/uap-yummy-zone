3d914f324109e2c7b05b7042b7133c4d30e6a48b
Muhammad Shuvo Khan <86629505+shuvokhan60@users.noreply.github.com>
2025-10-22 11:40:58 +0600

    simple change urls
    simple change urls

--- yummyzone/urls.py
+++ yummyzone/urls.py
@@ -4,7 +4,7 @@ URL configuration for yummyzone project.
 
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings                # ✅ এইটা যোগ করো
+from django.conf import settings               
 from django.conf.urls.static import static
 from . import views
 
